e7db3efe2927a2cf06af91cf11a858b8cbdb8476
MitchAguilar <madialeja06@gmail.com>
2017-08-28 18:14:37 -0500

    add new code

--- CalculadoraApi/src/Iniciar/Prinicipal.java
+++ CalculadoraApi/src/Iniciar/Prinicipal.java
@@ -2,12 +2,36 @@ package Iniciar;
 
 import Frames.CalApi;
 import Frames.CalNormal;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Prinicipal {
 
-    public static void main(String[] args) {
-        CalNormal A = new CalNormal();
+    public static void main(String[] args) throws MalformedURLException, IOException {
+//        CalNormal A = new CalNormal();
 //        CalApi B = new CalApi();
+        URL url = new URL("http://191.102.85.226/Mitch/Api/Register");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(url.openStream()));
+        File archivo = new File("Descargado.txt");
+        BufferedWriter bw = new BufferedWriter(new FileWriter(archivo));
+        String cadena;
+        String Response = null;
+        while ((cadena = bf.readLine()) != null) {
+            bw.write(cadena);
+            Response = cadena;
+        }
+        String que[]=Response.split(",");
+        for (int i = 0; i < que.length; i++) {
+            System.out.println(que[i]);
+        }
+        bf.close();
+        bw.close();
 
     }
 
