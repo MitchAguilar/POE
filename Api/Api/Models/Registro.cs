4caea6d5104eb531f7866c255b43b5f9c6c5c048
MitchAguilar <madialeja06@gmail.com>
2017-08-28 14:29:11 -0500

    add new method

---
+++ Api/Api/Models/Registro.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Api.Models
+{
+  public class Registro
+  {
+    public String Registro()
+    {
+      return
+    }
+  }
+}
