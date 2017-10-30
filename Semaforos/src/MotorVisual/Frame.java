6ec448aa9853b16977cc7b2c38c3905aca489e56
MitchAguilar <ma.aguilar@udla.edu.co>
2017-10-30 02:08:31 -0500

    add new updates

--- Semaforos/src/MotorVisual/Frame.java
+++ Semaforos/src/MotorVisual/Frame.java
@@ -10,6 +10,7 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.net.URL;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,50 +22,35 @@ import javax.swing.JPanel;
 public class Frame {
 
     JFrame Fram;
-    JPanel Cesped, Cesped2, Edific;
+    JPanel Matix;
     JLabel Arb;
-    static Imagen img;
+    JButton start;
 
     public void Components() {
-        Cesped = new JPanel();
-        Cesped.setBounds(0, 398, 450, 300);
-        Cesped.setLayout(null);
-        img = new Imagen("/Complements/Cesped.PNG", Cesped.getWidth(), Cesped.getHeight());
-        Cesped.add(img);
+        Matix = new JPanel();
+        Matix.setBounds(0, 0, 1024, 700);
+        Matix.setLayout(null);
+        Imagen img = new Imagen("/Complements/Fondo.jpg", Matix.getWidth(), Matix.getHeight());
+        Matix.add(img);
 
-        Cesped2 = new JPanel();
-        Cesped2.setBounds(550, 398, 700, 300);
-        Cesped2.setLayout(null);
-        Imagen img2 = new Imagen("/Complements/Cesped.PNG", Cesped2.getWidth(), Cesped2.getHeight());
-        Cesped2.add(img2);
+        start = new JButton();
+        URL url = this.getClass().getResource("/Complements/St.png");
+        start.setIcon(new ImageIcon(url));
+        start.setBorder(null);
+        start.setOpaque(false);
+        start.setBounds(1070, 150, 100, 100);
+        
 
-        Arb = new JLabel();
-        URL url = this.getClass().getResource("/Complements/Edificios.png");
-        Arb.setIcon(new ImageIcon(url));
-        Arb.setFocusable(false);
-        Arb.setBounds(850, 90, 560, 300);
-        Arb.setBorder(null);
-        Arb.setOpaque(false);
-
-//            Edific= new JPanel();
-//            Edific.setBounds(10,300, 260, 340);
-//            Imagen im= new Imagen("/Complements/Edificios.png", Edific.getWidth(), Edific.getHeight());
-//            Edific.add(im);
-//            Edific.repaint();
     }
 
     public void Fram() {
         Fram = new JFrame("Semaforos");
-        Fram.setSize(1024, 700);
+        Fram.setSize(1224, 700);
         Fram.setLayout(null);
 
-        Fram.add(Cesped);
-        Fram.add(Cesped2);
-//        Fram.add(Edific);
-        Fram.add(Arb);
-
-        Imagen img = new Imagen("/Complements/Fondo.jpg", Fram.getWidth(), Fram.getHeight());
-        Fram.add(img);
+//        Fram.setUndecorated(true);
+        Fram.add(Matix);
+        Fram.add(start);
         Image icon = Toolkit.getDefaultToolkit().getImage(getClass().getResource("/Complements/Icon.png"));
         Fram.setIconImage(icon);
 
